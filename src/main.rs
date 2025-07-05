5703b65c8fd6e82384dc923f254b64ddfa9f0bdd
Rowan <rowan.t.preston@gmail.com>
2025-07-05 09:38:04 +0100

    Initialise project for egui

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+mod app;
+
+use crate::app::TemplateApp;
+
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result {
+    //env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Ok(Box::new(TemplateApp::new(cc)))),
+    )
+}
